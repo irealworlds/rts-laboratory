0c65447e2f2f90f5cd42205eb78501bddf7e7b65
Marin Calin <marincalin2551@hotmail.com>
2023-04-20 14:10:49 +0300

    Added timestamp to message prints

--- src/Lab4/BaseExerciseThread.java
+++ src/Lab4/BaseExerciseThread.java
@@ -1,5 +1,8 @@
 package Lab4;
 
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+
 public class BaseExerciseThread extends Thread {
     protected void activity(int min, int max) {
         var k = (int) Math.round(Math.random() * (max - min) + min);
@@ -18,6 +21,7 @@ public class BaseExerciseThread extends Thread {
     }
 
     protected void printMessage(String message) {
-        System.out.println(this.getName() + " - " + message);
+        String timeStamp = new SimpleDateFormat("HH:mm:ss.SSS").format(new java.util.Date());
+        System.out.println(MessageFormat.format("[{0}] {1} - {2}", timeStamp, this.getName(), message));
     }
 }
